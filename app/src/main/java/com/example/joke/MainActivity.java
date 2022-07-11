24c23c8dfddb799f2952c41d11efb68a23367b11
SarcasticNickname <78605109+SarcasticNickname@users.noreply.github.com>
2022-07-11 23:00:27 +0300

    Notification, Notification channel.

--- app/src/main/java/com/example/joke/MainActivity.java
+++ app/src/main/java/com/example/joke/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.joke;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button button = findViewById(R.id.button);
         button.setOnClickListener(this::buttonOnClick);
     }
--- app/src/main/java/com/example/joke/SongPlayer.java
+++ app/src/main/java/com/example/joke/SongPlayer.java
@@ -1,6 +1,10 @@
 package com.example.joke;
 
 import android.app.IntentService;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
@@ -8,9 +12,12 @@ import android.provider.MediaStore;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class SongPlayer extends IntentService {
     private MediaPlayer player;
+    public static final String NOTE_CHANNEL_ID = "abcdefg";
+    public static final int NOTE_ID = 12345;
 
     public SongPlayer() {
         super("SongPlayer");
@@ -20,16 +27,31 @@ public class SongPlayer extends IntentService {
     protected void onHandleIntent(@Nullable Intent intent) {
         player = MediaPlayer.create(getApplicationContext(), R.raw.song);
         player.start();
+        notifyOnMusicStarted();
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (player != null) {
-            if (player.isPlaying()) {
-                player.stop();
-            }
-            player.release();
+    private void notifyOnMusicStarted() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, NOTE_CHANNEL_ID)
+                        .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                        .setContentTitle(getString(R.string.music_notification_title))
+                        .setContentText(getString(R.string.music_notification_text))
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setVibrate(new long[] {0, 1000})
+                        .setAutoCancel(true);
+        Intent actionIntent = new Intent(this, MainActivity.class);
+        PendingIntent actionPendingIntent = PendingIntent.getActivity(this,
+                0,
+                actionIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(actionPendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            channel = new NotificationChannel("abcdefg", "Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
         }
+        manager.notify(NOTE_ID, builder.build());
     }
 }
